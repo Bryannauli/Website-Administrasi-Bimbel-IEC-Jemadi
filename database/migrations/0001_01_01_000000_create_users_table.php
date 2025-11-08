dc47d7bb6615a3c0089564d244fc85343cb8ea8f
Agnes Olivia <agnesketaren94@gmail.com>
2025-11-08 10:47:55 +0700

    ganti ke breeze

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             // informasi tambahan (boleh kosong)
             $table->string('email')->unique()->nullable();
             $table->string('phone')->unique()->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             
             $table->string('password');
             $table->enum('role', ['admin', 'teacher']);
